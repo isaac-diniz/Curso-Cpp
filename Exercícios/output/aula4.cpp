26fa738b06b18fbde34669a7e018548e5bd99154
isaac-diniz <dinizisaac266@gmail.com>
2024-01-07 12:06:35 -0300

    aula 4 do curso, variáveis

---
+++ Exercícios/output/aula4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//tipo, nome, valorPadrão ("=" atribuir, inserir)
+	
+	int exemplos; //números inteiros
+	
+	char exemploLetra; //char => caractere, caractere 'B' com apóstrofro
+	//ex: char letras[10]"10 caracteres" é um vetor, armazena um texto, porém cada caractere em uma posição
+	
+	double exemploDecimal; //números reais
+	
+	float exemploDecimalMenos; //semelhante ao double, porém, menos preciso
+	//ex: double="4.99999", float="5.0"
+	
+	bool exemploSimples; //true=Verdadeiro=1 ou false=falso=0, booleano, sim ou não
+	//testes condicionais
+	
+	string exemploText; //diferente do char, armazena um texto ao invés de apenas um caractere
+	
+	return 0;
+}
