651928a7c47b26f077d3cf2cd97e61e7ca1a124a
isaac-diniz <dinizisaac266@gmail.com>
2024-01-08 10:30:46 -0300

    mudança de nome da pasta principal e criação da nova pasta, exerciciosLivro

---
+++ Exercícios/exercíciosLIvro/exer1.cpp
@@ -0,0 +1,29 @@
+/*
+ALGORITMO
+DECLARE n1, n2, n3, n4, soma NUMÉRICO
+LEIA n1, n2, n3, n4
+soma ? n1 + n2 + n3 + n4
+ESCREVA soma
+FIM_ALGORITMO.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n1, n2, n3, n4, soma;
+	
+	cout << "digite os 4 valores inteiros em sequecia: \n";
+	cin >> n1;
+	cin >> n2;
+	cin >> n3;
+	cin >> n4;
+	
+	soma = n1+n2+n3+n4;
+	
+	cout << "A soma dos valores e: " << soma;
+	
+	return 0;
+}
