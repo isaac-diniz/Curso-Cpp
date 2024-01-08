740b06cb00a55a82443cbabd4fa94761ee4871eb
isaac-diniz <dinizisaac266@gmail.com>
2024-01-07 22:51:27 -0300

    imprimindo as variáveis

--- Exercícios/output/aula4.cpp
+++ Exercícios/output/aula4.cpp
@@ -6,20 +6,27 @@ int main(){
 	
 	//tipo, nome, valorPadrão ("=" atribuir, inserir)
 	
-	int exemplos; //números inteiros
+	int exemplos=0; //números inteiros
 	
-	char exemploLetra; //char => caractere, caractere 'B' com apóstrofro
+	char exemploLetra='B'; //char => caractere, caractere 'B' com apóstrofro
 	//ex: char letras[10]"10 caracteres" é um vetor, armazena um texto, porém cada caractere em uma posição
 	
-	double exemploDecimal; //números reais
+	double exemploDecimal=0.1; //números reais
 	
-	float exemploDecimalMenos; //semelhante ao double, porém, menos preciso
+	float exemploDecimalMenos=0.1; //semelhante ao double, porém, menos preciso
 	//ex: double="4.99999", float="5.0"
 	
-	bool exemploSimples; //true=Verdadeiro=1 ou false=falso=0, booleano, sim ou não
+	bool exemploSimples=true; //true=Verdadeiro=1 ou false=falso=0, booleano, sim ou não
 	//testes condicionais
 	
-	string exemploText; //diferente do char, armazena um texto ao invés de apenas um caractere
+	string exemploText="Isaac"; //diferente do char, armazena um texto ao invés de apenas um caractere
+	
+	cout << exemplos << "\n";
+	cout << exemploLetra << "\n";
+	cout << exemploDecimal << "\n";
+	cout << exemploDecimalMenos << "\n";
+	cout << exemploSimples << "\n";
+	cout << exemploText << "\n";
 	
 	return 0;
 }
