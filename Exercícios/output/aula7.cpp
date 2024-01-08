0b823534fe30ac2fc5a41fd2cc4638443cd3729c
isaac-diniz <dinizisaac266@gmail.com>
2024-01-08 10:22:09 -0300

    fim aula 7

---
+++ Exercícios/output/aula7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//n1=n1+1;
+	//n1+=1;	
+	//n1++;  pós incremento/fixado
+	//++n1;  pré incremento/fixado
+	//n1*=n1;
+	
+	int n1=2;
+	
+	cout << n1++ <<"\n"; //usa a variavel, e depois faz o incremento
+	cout << n1 << "\n";
+	cout << ++n1 <<"\n"; //faz o incremento, e depois usa a variavel
+	
+	return 0;
+}	
