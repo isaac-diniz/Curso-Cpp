fac5f229f90aee6e29de600ed92dc336c3b03beb
isaac-diniz <dinizisaac266@gmail.com>
2024-01-08 11:16:46 -0300

    exercicio livro 4

---
+++ Exercícios/exercíciosLIvro/exer4.cpp
@@ -0,0 +1,20 @@
+/*
+Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se
+que este sofreu um aumento de 25%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float salario;
+	
+	cout << "insira o salario autal: ";
+	cin >> salario;
+	salario=salario*1.25;
+	cout << "o salaraio apos o aumento de 25% e: " << salario;
+	
+	return 0;
+}
