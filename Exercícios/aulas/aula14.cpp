e6736bf3548314aa7ee81a1b574b963d94e86e13
isaac-diniz <dinizisaac266@gmail.com>
2024-01-09 16:17:53 -0300

    aula 14 curso

---
+++ Exercícios/aulas/aula14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n=0;
+	//enquanto a condição dentro do parêntese for VERDADE o codigo é executado
+	while(n++<10)
+	{
+		cout<<n<<"\n";
+		//n++;
+		//terminaria o código sempre com 1 a mais do seu valor incial do loop em questão
+	}
+	
+	return 0;
+}
