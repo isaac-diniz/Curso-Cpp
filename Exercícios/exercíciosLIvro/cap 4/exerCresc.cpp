c1aff1c584b0d68b539b96519d09e35c2b35ebc0
isaac-diniz <dinizisaac266@gmail.com>
2024-01-12 18:28:02 -0300

    exercicio ordem crescente

---
+++ Exercícios/exercíciosLIvro/cap 4/exerCresc.cpp
@@ -0,0 +1,31 @@
+/*
+Solicite ao usuário para inserir três números e os exiba em ordem crescente.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    double n1,n2,n3,temp;
+    cout<<"Digite o primeiro número: ";
+    cin>>n1;
+    cout<<"Digite o segundo número: ";
+    cin>>n2;
+	cout<<"Digite o terceiro número: ";
+    cin>>n3;
+    if(n1>n2){
+        temp=n1;
+        n1=n2;
+        n2=temp;
+    }
+    if(n2>n3){
+        temp=n2;
+        n2=n3;
+        n3=temp;
+    }
+    if(n1>n2){
+        temp=n1;
+        n1=n2;
+        n2=temp;
+    }
+	cout<<n1<<", "<<n2<<", "<< n3;
+    return 0;
+}
