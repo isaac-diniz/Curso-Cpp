14d213a2b7f0cd7238a065cdbdc268d3bcf51278
isaac-diniz <dinizisaac266@gmail.com>
2024-01-27 18:52:40 -0300

    Exercício 4 - cap 7

--- Exercícios/exercíciosLIvro/cap 7/exer4.cpp
+++ Exercícios/exercíciosLIvro/cap 7/exer4.cpp
@@ -10,11 +10,31 @@ ou exame);
 #include <iostream>
 using namespace std;
 int main(){
+    string name[15]={" "}, sit[15];
+    float nota[15][5], media[15]={0}, tot=0;
     for(int i=0; i<15; i++){
+        cout<<"\nInsira o nome do aluno: ";
+        cin>>name[i];
+        cout<<"\n\nAluno: "<<name[i];
         for (int z=0; z<5; z++){
-            cin
+            cout<<"\nInsira a "<<z+1<<" nota do aluno: ";
+            cin>>nota[i][z];
+            media[i]+=nota[i][z];
         }
-        
+        media[i]=media[i]/5;
+        if(media[i]>=7){
+            sit[i]="Aprovado";
+        }else if(media[i]>=3){
+            sit[i]="Exame";
+        }else{
+            sit[i]="Reprovado";
+        }
+        tot+=media[i];
     }
+    for(int i=0; i<15; i++){
+        cout<<"\nSITUACAO DO ALUNO: "<<name[i]<<endl;
+        cout<<"Media: "<<media[i]<<"\nSituacao: "<<sit[i]<<endl;
+    }
+    cout<<"\n\nMedia da turma: "<<tot/15;
     return 0;
 }
