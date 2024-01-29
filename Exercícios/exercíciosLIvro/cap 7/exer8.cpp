0f9e5428bf74b80d550715a2b031f9bb0a338d41
isaac-diniz <dinizisaac266@gmail.com>
2024-01-29 12:47:17 -0300

    exercício 8 - cap 7 (otimização de código

--- Exercícios/exercíciosLIvro/cap 7/exer8.cpp
+++ Exercícios/exercíciosLIvro/cap 7/exer8.cpp
@@ -6,17 +6,13 @@ Crie um programa que preencha duas matrizes 3  8 com números inteiros, calcu
 #include <iostream>
 using namespace std;
 int main(){
-    float matr1[3][8], matr2[3][8], result1[3][8], result2[3][8];
+    int matr1[3][8], matr2[3][8], result1[3][8], result2[3][8];
     for(int i=0; i<3; i++){
         for(int z=0; z<8; z++){
             cin>>matr1[i][z];
             cin>>matr2[i][z];
             result1[i][z]=matr1[i][z]+matr2[i][z];
-            if(matr1[i][z]>=matr2[i][z]){
-                result2[i][z]=matr1[i][z]-matr2[i][z];
-            }else{
-                result2[i][z]=matr2[i][z]-matr1[i][z];
-            }
+            result2[i][z]=abs(matr1[i][z]-matr2[i][z]);
         }
     }
     cout<<"resultante 1:\n";
---
+++ Exercícios/exercíciosLIvro/cap 7/exer9.cpp
@@ -0,0 +1,5 @@
+/*
+Faça um programa que preencha uma matriz 3  3 com números reais e outro valor numérico digitado pelo 
+usuário. O programa deverá calcular e mostrar a matriz resultante da multiplicação do número digitado por 
+cada elemento da matriz. 
+*/
