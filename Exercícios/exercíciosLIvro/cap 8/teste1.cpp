0d6d562793e57f6a6e50344a184763d9fde80467
isaac-diniz <dinizisaac266@gmail.com>
2024-01-31 12:56:37 -0300

    capítulo 8 - subrotinas

---
+++ Exercícios/exercíciosLIvro/cap 8/teste1.cpp
@@ -0,0 +1,15 @@
+/*
+ 1. ALGORITMO
+ 2. DECLARE sal, aum, novo_sal NUMÉRICO
+ 3. LEIA sal
+ 4. aum ← calculo (sal)
+ 5. novo_sal ← sal + aum
+ 6. ESCREVA “Novo salário é”, novo_sal
+ 7. FIM_ALGORITMO.
+ 8. SUB-ROTINA calculo (sal NUMÉRICO) // passagem de parâmetro por valor
+ 9.  DECLARE perc, valor NUMÉRICO
+ 10.  LEIA perc 
+ 11.  valor ←  sal * perc / 100 
+ 12.  RETORNE valor 
+ 13. FIM_SUB_ROTINA calculo 
+*/
