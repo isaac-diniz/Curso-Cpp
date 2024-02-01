270eb09c606b78d7fa8b637abe8bdff4ed7c3beb
isaac-diniz <dinizisaac266@gmail.com>
2024-02-01 12:54:03 -0300

    teste 1 - subrotinas

--- Exercícios/exercíciosLIvro/cap 8/teste1.cpp
+++ Exercícios/exercíciosLIvro/cap 8/teste1.cpp
@@ -13,3 +13,21 @@
  12.  RETORNE valor 
  13. FIM_SUB_ROTINA calculo 
 */
+#include <stdio.h>
+void soma(int a, int b)
+{ 
+    int s;  // variável local da sub-rotina soma
+    s = a + b;
+    printf("\nSoma = %d",s); 
+} 
+int main() 
+{ 
+   int a, b;  // variáveis locais da sub-rotina main
+   printf("\nDigite o primeiro numero: ");
+   scanf("%d%*c",&a);
+   printf("\nDigite o segundo numero: ");
+   scanf("%d%*c",&b);
+   soma(a,b); 
+   getchar();
+   return 0;
+}
