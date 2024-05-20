17616c8bf2cb34d5a169602d044f9a65a67f07ee
isaac-diniz <dinizisaac266@gmail.com>
2024-05-20 13:31:43 -0300

    omissãoo de argumentos

---
+++ aulas/aulascpp/aula24.cpp
@@ -0,0 +1,31 @@
+/*
+omissão de argumentos ou valores padrões para argumentos dentro de uma função
+criação da possibilidade de omissão desses argumentos
+
+ex: função com 3 argumentos e eu uso somente um, ou nenhum
+*/
+#include<iostream>
+using namespace std;
+
+/*
+para fazer isso, basta passar um valor padrão para o(s) argumento(s) da função
+segue o exemplo
+*/
+void example(string text="\nexemplo");
+/*
+dessa forma, não necessariamente, precisa ser atribuido um valor a tal função
+*/
+
+int main(){
+
+    example();
+    //saída: \nexemplo\n
+    example("o valor interno passado pela chamada sobressai o valor padrão dos argumentos.");
+    //saída: o valor interno passado pela chamada sobressai o valor padrão dos argumentos.
+
+    return 0;
+}
+
+void example(string text){
+    cout<<text<<endl;
+}
