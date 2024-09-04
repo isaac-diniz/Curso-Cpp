4470ffee523321a8c8e92ad7135bc9ee1029da30
isaac-diniz <dinizisaac266@gmail.com>
2024-09-04 20:56:04 -0300

    funções

--- Exercícios/exercíciosAdicionais/continue.cpp
+++ Exercícios/exercíciosAdicionais/continue.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main(){
     for(int i=0;i<10;i++){
         if(i==2) continue;
+        //continue: pula o restante da repetiçao de caso n para o proximo caso n1.
+        if(i==6) break;
+        //break: encerra o processo de repetição.
         cout<<i<<endl;
     }
     return 0;
